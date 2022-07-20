b9d93d7f89dd33a9a47e9beb2f6cfb743c286998
Oein <oein0219@gmail.com>
2022-07-20 20:01:43 +0900

    v1

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <></>;
+import { useRouter } from "next/router";
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function L() {
+  let router = useRouter();
+
+  useEffect(() => {
+    axios.get("/api/link/get").then((d) => {
+      let dt = d.data;
+      router.push(decodeURI(dt));
+    });
+  });
+
+  return (
+    <>
+      <h1>Wait a moment</h1>
+      <h2>
+        I&apos;m redirecting you to Heungo Elementry school&apos;s broadcast
+        link.
+      </h2>
+    </>
+  );
 }
