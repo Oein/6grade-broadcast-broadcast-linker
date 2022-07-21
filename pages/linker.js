1dd68dba45f62c91c15c8680c8bc1437fff00356
Oein <oein0219@gmail.com>
2022-07-21 14:47:27 +0900

    Change ui

--- pages/linker.js
+++ pages/linker.js
@@ -3,19 +3,37 @@ import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Load from "../components/loading";
+import { Input, Button, Tooltip } from "@nextui-org/react";
 
 export default function Linker() {
   const [ld, setL] = useState(false);
   const [ip, setI] = useState("");
 
   return (
-    <>
+    <div
+      style={{
+        top: "8vh",
+        bottom: "8vh",
+        width: "90vw",
+        height: "92vh",
+        left: "5vw",
+        right: "5vw",
+        position: "fixed",
+      }}
+    >
+      <header
+        style={{
+          marginBottom: "30px",
+        }}
+      >
+        <h1>Oein's Heungo Broadcast Linker</h1>
+      </header>
       <ToastContainer position="bottom-left" autoClose={3000} />
       {ld ? <Load /> : null}
-      <input
-        placeholder="Put Broadcast Link here"
+      <Input
+        labelPlaceholder="Put Broadcast Link here"
         style={{
-          width: "90vw",
+          width: "80vw",
           fontSize: "1.1rem",
         }}
         value={ip}
@@ -25,32 +43,43 @@ export default function Linker() {
       />
       <p></p>
 
-      <button
-        onClick={() => {
-          setL(true);
-          axios
-            .get(`/api/link/set`, {
-              params: {
-                url: encodeURI(ip),
-              },
-            })
-            .then(() => {
-              toast("Successfully linked!", {
-                type: "success",
-              });
-            })
-            .catch(() => {
-              toast("Error occured", {
-                type: "error",
-              });
-            })
-            .finally(() => {
-              setL(false);
-            });
-        }}
+      <Tooltip
+        content={"This will link hdyt.kro.kr to url above"}
+        rounded
+        color="primary"
+        placement="bottom"
       >
-        Link!
-      </button>
-    </>
+        <Button
+          style={{
+            marginTop: "100px",
+          }}
+          color="success"
+          onClick={() => {
+            setL(true);
+            axios
+              .get(`/api/link/set`, {
+                params: {
+                  url: encodeURI(ip),
+                },
+              })
+              .then(() => {
+                toast("Successfully linked!", {
+                  type: "success",
+                });
+              })
+              .catch(() => {
+                toast("Error occured", {
+                  type: "error",
+                });
+              })
+              .finally(() => {
+                setL(false);
+              });
+          }}
+        >
+          Link!
+        </Button>
+      </Tooltip>
+    </div>
   );
 }
