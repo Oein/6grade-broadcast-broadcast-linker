2f5846cb94b91d93d72447bf2523c2937ec2947d
Oein <oein0219@gmail.com>
2022-07-21 14:58:29 +0900

    Change ui

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
 import { useRouter } from "next/router";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function L() {
   let router = useRouter();
+  let [st, sst] = useState("서버와 통신중..");
+  let [lt, slt] = useState(false);
 
   useEffect(() => {
-    axios.get("/api/link/get").then((d) => {
-      let dt = d.data;
-      router.push(decodeURI(dt));
-    });
+    if (!lt) {
+      axios.get("/api/link/get").then((d) => {
+        let dt = d.data;
+        sst(`링크 '${dt}' 로 이동중!`);
+        router.push(decodeURI(dt));
+      });
+      slt(true);
+    }
   });
 
   return (
-    <>
-      <h1>Wait a moment</h1>
-      <h2>
-        I&apos;m redirecting you to Heungo Elementry school&apos;s broadcast
-        link.
-      </h2>
-    </>
+    <header
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1>잠시만 기다려 주세요</h1>
+      <desc>흥도초등학교 유튜브 스트리밍 링크로 이동중 입니다!</desc>
+      <p></p>
+      <desc>{st}</desc>
+    </header>
   );
 }
