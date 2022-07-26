3749613f470dce53b269eac006cbda9bd9750337
Oein <oein0219@gmail.com>
2022-07-26 20:17:08 +0900

    Add error handler

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,16 @@ export default function L() {
 
   useEffect(() => {
     if (!lt) {
-      axios.get("/api/link/get").then((d) => {
-        let dt = d.data;
-        sst(`링크 '${dt}' 로 이동중!`);
-        router.push(decodeURI(dt));
-      });
+      axios
+        .get("/api/link/get")
+        .then((d) => {
+          let dt = d.data;
+          sst(`${st}${"\n"}링크 '${dt}' 로 이동중!`);
+          router.push(decodeURI(dt));
+        })
+        .catch((e) => {
+          sst(`${st}${"\n"}Error 발생${"\n"}${e.toString()}`);
+        });
       slt(true);
     }
   });
@@ -27,7 +32,11 @@ export default function L() {
       <h1>잠시만 기다려 주세요</h1>
       <desc>흥도초등학교 유튜브 스트리밍 링크로 이동중 입니다!</desc>
       <p></p>
-      <desc>{st}</desc>
+      <desc>
+        {st.split("\n").map((v) => {
+          return <p>{v}</p>;
+        })}
+      </desc>
     </header>
   );
 }
